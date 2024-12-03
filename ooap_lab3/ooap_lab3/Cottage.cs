0098fdabbbe4ebb0bcb03852bcc9f936752896b0
JamesGar13 <134848100+JamesGar13@users.noreply.github.com>
2024-12-03 10:47:53 +0200

    Add files via upload

---
+++ ooap_lab3/ooap_lab3/Cottage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ooap_lab3
+{
+    public class Cottage : House
+    {
+        public string Owner { get; set; }
+
+        public Cottage(double area, int floors, string address, string owner)
+        {
+            Area = area;
+            Floors = floors;
+            Address = address;
+            Owner = owner;
+        }
+
+        public override House Clone()
+        {
+            return new Cottage(this.Area, this.Floors, this.Address, this.Owner);
+        }
+
+        public override void DisplayInfo()
+        {
+            Console.WriteLine($"Cottage Building: Area={Area}, Floors={Floors}, Address={Address}, Owner={Owner}");
+        }
+    }
+}
---
+++ ooap_lab3/ooap_lab3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ooap_lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<House> houses = new List<House>();
+
+            Apartment apartment = new Apartment(300.0, 2, "221b Baker Street", new List<string> 
+            { "Sherlock Holmes", "John Watson" });
+            Cottage cottage = new Cottage(200.0, 1, "4 Privet Drive", "Harry Potter");
+
+            houses.Add(apartment);
+            houses.Add(cottage);
+
+            Apartment clonedApartment = (Apartment)apartment.Clone();
+            clonedApartment.Address = "Changed Address";
+            clonedApartment.ApartmentOwners.Add("Rig");
+            houses.Add(clonedApartment);
+
+            Console.WriteLine("All Houses:");
+            foreach (var house in houses)
+            {
+                house.DisplayInfo();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Editing house information...");
+            if (houses[0] is Apartment editedApartment)
+            {
+                editedApartment.Address = "Updated Address";
+                editedApartment.ApartmentOwners[0] = "Anonymous";
+            }
+
+            Console.WriteLine("\nUpdated Houses:");
+            foreach (var house in houses)
+            {
+                house.DisplayInfo();
+                Console.WriteLine();
+            }
+        }
+    }
+}
